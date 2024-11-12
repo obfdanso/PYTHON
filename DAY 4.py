ab373a9109167bc75dd0ccdf144d73b182b638c5
obfdanso <ddanso3000@gmail.com>
2024-11-12 08:49:15 +0000

    Initial commit

--- DAY 4.py
+++ DAY 4.py
@@ -1,29 +1,29 @@
 import random
-# integer= random.randint(0,1)
+integer= random.randint(0,1)
 
-# if integer == 0:
-#    print("Head")
-# else:
-#    print("Tail")
+if integer == 0:
+   print("Head")
+else:
+   print("Tail")
 
-# names= input("Give me the names of everyone separated by a comma. ")
-# name= names.split(", ")
-# length= len(name)
-# choice=random.randint(0, length-1)
-# pay_person= name[choice]
-# print(f"{pay_person} pays")
+names= input("Give me the names of everyone separated by a comma. ")
+name= names.split(", ")
+length= len(name)
+choice=random.randint(0, length-1)
+pay_person= name[choice]
+print(f"{pay_person} pays")
 
-# row1= ["😀", "😄", "😇"]
-# row2= ["😗", "🤪", "😎"]
-# row3= ["🥺", "☹️", "👻"]
-# map= [row1,row2,row3]
-# print(f"{row1}\n {row2}\n {row3}")
-# position= input("Where do you want to put the treasure? ") 
+row1= ["😀", "😄", "😇"]
+row2= ["😗", "🤪", "😎"]
+row3= ["🥺", "☹️", "👻"]
+map= [row1,row2,row3]
+print(f"{row1}\n {row2}\n {row3}")
+position= input("Where do you want to put the treasure? ") 
 
-# horizontal= int (position[0])
-# vertical = int(position[1])
-# map [vertical-1][horizontal-1]= "X"
-# print(f"{row1}\n {row2}\n {row3}")
+horizontal= int (position[0])
+vertical = int(position[1])
+map [vertical-1][horizontal-1]= "X"
+print(f"{row1}\n {row2}\n {row3}")
 
 
 
--- DAY 7.py
+++ DAY 7.py
@@ -1,47 +1,48 @@
- # for letter in chosen_word:
-    #     if letter == guess:
-    #         print("Right")
-    #     else:
-    #         print("Wrong")
-# import random
+import random
+logo = '''                                         
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/ '''                          
+print(logo)
 
-# logo = '''                                         
-# | |                                            
-# | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-# | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-# | | | | (_| | | | | (_| | | | | | | (_| | | | |
-# |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-#                     __/ |                      
-#                    |___/ '''                          
-# print(logo)
+word_list = ['Hypnosis','Baguette','Pterodactyl','Archaeology', 'Fibonacci', 'Labyrinth','Xenotransplantation','Pseudopseudohypoparathyroidism','Pickle','Python']
 
-# word_list = ['Hypnosis','Baguette','Pterodactyl','Archaeology', 'Fibonacci', 'Labyrinth','Xenotransplantation','Pseudopseudohypoparathyroidism','Pickle','Python']
+chosen_word = random.choice(word_list)
+display = []
+hint = random.choice(chosen_word).lower()
 
-# chosen_word = random.choice(word_list)
-# display = []
-# hint = random.choice(chosen_word).lower()
+for d in chosen_word:
+    display += "_"
+print(f"This is your hint {hint}, {display}")
+lives = len(chosen_word)
+endgame = False
 
-# for d in chosen_word:
-#     display += "_"
-# print(f"This is your hint {hint}, {display}")
-# lives = len(chosen_word)
-# endgame = False
+while not endgame:
+     while lives > 0:
+        guess = input("Guess a letter in the chosen word\n").lower()
+        letter = chosen_word[0]
 
-# while not endgame:
-#      while lives > 0:
-#         guess = input("Guess a letter in the chosen word\n").lower()
-#         letter = chosen_word[0]
+        for letter in chosen_word:
+            if letter == guess:
+                print("Right")
+            else:
+                print("Wrong")
+
        
-#         for position in range(len(chosen_word)):
-#             letter = chosen_word[position]
-#             if letter == guess:
-#                 display[position] = letter
-#         print(display)
-#         if guess not in chosen_word:
-#             lives -= 1
-#             print(f"You guessed {guess}, which is wrong. You lose a life")
-#         if lives == 0:
-#             print(f"You lose. The word was {chosen_word}")
-#         if "_" not in display:
-#             endgame = True
-#             print("You win")
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+        print(display)
+        if guess not in chosen_word:
+            lives -= 1
+            print(f"You guessed {guess}, which is wrong. You lose a life")
+        if lives == 0:
+            print(f"You lose. The word was {chosen_word}")
+        if "_" not in display:
+            endgame = True
+            print("You win")
