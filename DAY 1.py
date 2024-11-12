e81792020e3c808c4a984ecaf0714221b547ee88
obfdanso <ddanso3000@gmail.com>
2024-11-12 07:22:17 +0000

    First commit

---
+++ DAY 1.py
@@ -0,0 +1,4 @@
+print("Hello, welcome to the Band generator name")
+town= input("What is the town you grew up in?\n")
+pet_name=input("What is the name of your pet?\n")
+print("Your band name is", town,pet_name)
