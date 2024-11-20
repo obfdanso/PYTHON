1c3392b0811060fd8c40cfa89155a4e60faeadb1
obfdanso <ddanso3000@gmail.com>
2024-11-20 20:39:01 +0000

    Debugged file

---
+++ DAY 13.py
@@ -0,0 +1,66 @@
+#  Debugging
+    # def my_function():
+        #     for i in range(1, 21):
+        #         if i == 20:
+        #             print("You got it")
+        # my_function()
+
+    # from random import randint
+    # dice_imgs = ['1','2','3','4','5','6']
+    # dice_nums = randint(0, 5)
+    # print(dice_imgs[dice_nums])
+
+    # year = int(input("What's your year of birth? "))
+    # if year > 1980 and year < 1995:
+    #     print("You are a millenial.")
+    # elif year > 1994:
+    #     print("You are a Gen Z.")
+
+    # age = int(input("How old are you? "))
+    # if age > 18:
+    #     print(f"You can drive at age {age}")
+
+    # pages = 0
+    # word_per_page = 0
+    # pages = int(input("Number of pages: "))
+    # word_per_page = int(input("Number of words per page:"))
+    # total_words = pages * word_per_page
+    # print(total_words)
+
+    # def mutate(a_list):
+        #     b_list = []
+        #     for item in a_list:
+        #         new_item = item * 2
+        #         b_list.append(new_item)
+        #     print(b_list)
+        # mutate([1,2,3,5,8,13])
+
+    # number = int(input("What number do you want to check? "))
+
+    # if number % 2 == 0:
+    #     print("This is an even number")
+    # else:
+    #     print("This is an odd number")
+
+    # year = int(input("Which year do you want to check? "))
+
+    # if year % 4 == 0:
+    #     if year % 100 == 0:
+    #         if year % 400 == 0:
+    #             print("Leap year")
+    #         else:
+    #             print("Not leap year")
+    #     else:
+    #         print("Leap year")
+    # else:
+    #     print("Not leap year")
+
+    # for number in range(1,101):
+    #     if number % 3 == 0 and number % 5 == 0:
+    #         print("FizzBuzz")
+    #     elif number % 3 == 0:
+    #         print("Fizz")
+    #     elif number % 5 == 0:
+    #         print("Buzz")
+    #     else:
+    #         print(number)
