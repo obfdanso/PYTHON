eb52ff03380b46332344762136836eb5f812ff97
obfdanso <ddanso3000@gmail.com>
2024-11-27 15:56:10 +0000

    Initial commit

---
+++ DAY 16.py
@@ -0,0 +1,18 @@
+# # import turtle
+# # from turtle import Screen
+# #
+# # jerry = turtle.Turtle()
+# # jerry.shape("turtle")
+# # jerry.color("red")
+# # jerry.forward(100)
+# #
+# # my_screen = Screen()
+# # print(my_screen.bgcolor())
+# # my_screen.exitonclick()
+#
+# import prettytable
+# table = prettytable.PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electricity", "Water", "Fire"])
+# table.align = "l"
+# print(table)
